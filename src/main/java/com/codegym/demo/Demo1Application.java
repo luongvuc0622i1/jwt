6de29164f25d29a2882c6ae96b76157f26a48266
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-01-03 20:36:04 +0700

    comparable || compareTo

--- src/main/java/com/codegym/demo/Demo1Application.java
+++ src/main/java/com/codegym/demo/Demo1Application.java
@@ -1,13 +1,33 @@
 package com.codegym.demo;
 
+import com.codegym.demo.compare.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @SpringBootApplication
 public class Demo1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo1Application.class, args);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("John", 28));
+        studentList.add(new Student("Abc", 15));
+        studentList.add(new Student("Xohn", 21));
+
+        Collections.sort(studentList, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
     }
 
 }
